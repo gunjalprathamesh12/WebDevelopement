1e937c8caae6cd8060910a3f9942c4beca79f599
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-02-26 22:28:13 +0530

    Javascript

---
+++ JavaScript/Lec1/Datatypes/datattype.js
@@ -0,0 +1,11 @@
+var a=10;
+console.log(typeof a);
+
+let b = 2.0;
+console.log(typeof b);
+var name = "Prathamesh";
+console.log(typeof name);
+
+var aa=true;
+console.log(typeof aa);  
+/*Variables are used to store the value*/
