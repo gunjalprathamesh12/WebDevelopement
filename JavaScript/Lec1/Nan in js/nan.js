7b267bf270b82edfa933cd9f368a5d478109c3c1
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-02-27 21:27:06 +0530

    Javascriptn NaN(Not a Number)

---
+++ JavaScript/Lec1/Nan in js/nan.js
@@ -0,0 +1,9 @@
+var a = 0 / 0;
+console.log(a);
+console.log(typeof a);
+
+// NaN (not a number) it is used to represent a value which is not a valid number.
+// In JavaScript, NaN is a property of the global object
+
+console.log(isNaN(a));
+console.log(isNaN(10));
