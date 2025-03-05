a76cdcb5bdfedf6c90d034f4d008355e865abfad
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-05 21:20:46 +0530

    JavaScript String

---
+++ JavaScript/Lec1/Js String/str.js
@@ -0,0 +1,15 @@
+let a="Prathamesh";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);  // a[2] is 
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
+console.log(a[10]); // a[10] is undefined
+
+console.log(a.charAt(0));
+console.log(a.lenght());
