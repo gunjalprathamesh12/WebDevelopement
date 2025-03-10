2035751aa3418af3ae896f02bd0a63ef2e24bd61
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-10 06:22:44 +0530

    Templete literals

---
+++ JavaScript/lec2/Templete Literals/script.js
@@ -0,0 +1,9 @@
+let a=10;
+let b=20;
+let sum=a+b;
+console.log(`The sum of ${a} and ${b} is ${sum}`);
+// Output: The sum of 10 and 20 is 30
+
+let pencile=10;
+let eraser=5;
+console.log(`The Price of Pencile is ${pencile} and the price of eraser is ${eraser}`);
