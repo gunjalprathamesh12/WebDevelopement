859671dc73fdd491aced569a3f001d05e816c69d
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-15 21:06:23 +0530

    Protfolio

--- JavaScript/Questions/Good String or not/tempCodeRunnerFile.js
+++ JavaScript/Questions/Good String or not/tempCodeRunnerFile.js
@@ -1,5 +1,5 @@
-let str = "epple";
-if (str[0] == "a" || (str[0] == "A" && str.length >= 3)) {
+let str = "apple";
+if (str[0] == "a" || (Str[0] == "A" && str.length >= 3)) {
   console.log("Good String");
 } else {
   console.log("Bad String");
