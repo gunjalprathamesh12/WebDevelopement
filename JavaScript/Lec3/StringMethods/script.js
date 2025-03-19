199475a919c77576717f0b3618e1fde07d9a0908
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-19 20:18:04 +0530

    Js

---
+++ JavaScript/Lec3/StringMethods/script.js
@@ -0,0 +1,9 @@
+let str = "     Prathamesh     ";
+console.log(str);
+console.log(str.trim());
+// Output: Prathamesh
+
+let pass = prompt("Enter your password");
+let email= prompt("Enter your email");
+document.getElementById("pass").innerHTML=pass;
+document.getElementById("em").innerHTML=email;
