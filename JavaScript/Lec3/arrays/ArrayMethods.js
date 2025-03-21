45fcb657cd39dc6d7ecba98d17382a6381a52795
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:38:52 +0530

    Js Arrays

---
+++ JavaScript/Lec3/arrays/ArrayMethods.js
@@ -0,0 +1,15 @@
+let cars = ["Audi", "BMW", "TATA", "LamborGini"];
+
+cars.push("Bentley");
+cars.push("Ferrari");
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.pop();
+console.log(cars);
--- JavaScript/Lec3/arrays/tempCodeRunnerFile.js
+++ JavaScript/Lec3/arrays/tempCodeRunnerFile.js
@@ -1,11 +1,3 @@
-let fruits = ["mango", "banana", "apple", "papaya"];
+cars.pop();
+console.log(cars);
 
-console.log(fruits);
-console.log(fruits[0]);
-// Arrays ARE MUTABLE we can Change its values
-
-let fruit = "apple";
-console.log(fruit.replace("apple", "banana"));
-console.log(fruit);
-
-// Strings are IMMUTABLE IN JS
