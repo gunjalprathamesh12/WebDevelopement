35e46282d5d8c50eb6f9a48efc62cc2a29e3b20b
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 17:15:39 +0530

    Js

---
+++ JavaScript/Lec3/StringMethods/StringReplaceFn.js
@@ -0,0 +1,13 @@
+let str="i love coding";
+console.log(str.replace("love","hate"));
+// Output: i hate coding
+//
+// The replace() method replaces a specified value with another value in a string:
+// The replace() method does not change the string it is called on. It returns a new string.
+// By default, the replace() method replaces only the first match:
+
+let n="Prathamesh Gunjal";
+console.log(n.repeat(3));
+
+let fruit="MAngo";
+console.log(fruit.repeat(1000));
