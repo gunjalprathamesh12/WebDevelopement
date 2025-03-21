b335801ed68419ab418d50f08d5c45dd5d492e4b
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:14:44 +0530

    Js Arrays

---
+++ JavaScript/Lec3/arrays/arrays.js
@@ -0,0 +1,11 @@
+let stud = ["pranav", "Gunjal", "Rahul", "Raj", "Rahul"];
+console.log(stud);
+console.log(stud[0]);
+console.log(stud[1]);
+console.log(stud[2]);
+console.log(stud[3]); //Accessing the elements of the array
+console.log(stud[4]);
+console.log(stud.length);
+
+console.log(stud.length - 1); //Accessing the last element of the array
+console.log(stud[stud.length - 1]);
