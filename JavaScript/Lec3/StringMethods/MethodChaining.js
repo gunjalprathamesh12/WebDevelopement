f25c39968bfbbcc36598b095d6a2fcdbdd564ed0
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 17:02:35 +0530

    Js

---
+++ JavaScript/Lec3/StringMethods/MethodChaining.js
@@ -0,0 +1,5 @@
+let atr="               PRANAV             ";
+console.log(atr.trim().toLowerCase().startsWith("pranav"));
+// Output: true
+
+/*Method chaining is a programming technique in which multiple methods are called on the same object in a single statement. Each method call returns an object, allowing the next method to be called on that object. This technique is often used to write more concise and readable code. */
