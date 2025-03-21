f175149e8de59fda3a3850471bd7280a620407eb
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:23:55 +0530

    Js Arrays

---
+++ JavaScript/Lec3/arrays/arrayexp.js
@@ -0,0 +1,12 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(nums);
+console.log(nums[0]);
+console.log(nums[1]);
+console.log(nums.length);
+
+let data = ["prathamesh", 18, 20000];
+console.log(data);
+console.log(data[0][0]);
+
+let emptyarray = [];
+console.log(emptyarray.length);
