1215ac799fa493f591c1d784ff85fd6ff155c3eb
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:26:47 +0530

    Mutable arrays

---
+++ JavaScript/Lec3/arrays/MutableArrays.js
@@ -0,0 +1,11 @@
+let fruits = ["mango", "banana", "apple", "papaya"];
+
+console.log(fruits[0]);
+
+// Arrays ARE MUTABLE we can Change its values
+
+let fruit = "apple";
+console.log(fruit.replace("apple", "banana"));
+console.log(fruit);
+
+// Strings are IMMUTABLE IN JS
