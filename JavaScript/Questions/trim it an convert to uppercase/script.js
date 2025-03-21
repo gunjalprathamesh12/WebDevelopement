46098c6cb2f684c3b12fea6887360df2442871a1
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:10:00 +0530

    Js

---
+++ JavaScript/Questions/trim it an convert to uppercase/script.js
@@ -0,0 +1,9 @@
+let msg="   help!   ";
+console.log(msg.trim().toUpperCase());
+
+
+let str="ApnaCollege";
+console.log(str.slice(4,9));
+console.log(str.indexOf("na"));
+console.log(str.repeat(3));
+console.log(str.replace("College","University"));
