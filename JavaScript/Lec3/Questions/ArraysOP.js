4e11119792155cc5438668ca5ad5e7acf2e75eb9
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:45:22 +0530

    Js Arrays

---
+++ JavaScript/Lec3/Questions/ArraysOP.js
@@ -0,0 +1,12 @@
+let months = ["january", "February", "March", "April", "May", "June"];
+console.log(months);
+
+months.pop();
+console.log(months);
+
+let month = ["january", "July", "May", "June"];
+month.shift();
+month.shift();
+month.unshift("March");
+month.unshift("December");
+console.log(month);
