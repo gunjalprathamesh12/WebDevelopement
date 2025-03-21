97c99674381ac9a5219c4fa5c6de6991343a256b
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 20:47:10 +0530

    Js Arrays

---
+++ JavaScript/Lec3/Questions/IndexOf.js
@@ -0,0 +1,7 @@
+let arr = ["Yellow", "Red", "Blue", "Green"];
+console.log(arr);
+
+console.log(arr.indexOf("Yellow"));
+console.log(arr.indexOf("Red"));
+console.log(arr.indexOf("Blue"));
+console.log(arr.indexOf("Green"));
