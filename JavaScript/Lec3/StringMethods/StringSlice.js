d013331d892e5cca095629a8391a8a283cd4b667
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-21 17:10:22 +0530

    Js

---
+++ JavaScript/Lec3/StringMethods/StringSlice.js
@@ -0,0 +1,11 @@
+let str="Prathamesh Gunjal";
+let str1=str.slice(0,10);
+console.log(str1);
+
+let str2="Prathamesh Gunjal Web Developer";
+console.log(str2.slice(0,str2.length));
+
+let pranav="Pranav Gunjal";
+console.log(pranav.slice(-7).trim().toUpperCase());//method chaining
+
+// Output: Gunjal
