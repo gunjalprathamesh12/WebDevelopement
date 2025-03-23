494aaf37db31bddab12b37b28827bf5445607d3e
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-23 15:26:28 +0530

    Js Arrays

--- JavaScript/Lec3/Questions/IndexOf.js
+++ JavaScript/Lec3/Questions/IndexOf.js
@@ -6,3 +6,6 @@ console.log(arr.indexOf("Red"));
 console.log(arr.indexOf("Blue"));
 console.log(arr.indexOf("Green"));
 console.log(arr.indexOf("red"));
+
+// Output:
+// [ 'Yellow', 'Red', 'Blue', 'Green' ]
