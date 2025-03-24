3471e91d0854720c74f9f2d69ccca628d1dd4173
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-24 17:13:04 +0530

    Js Arrays

---
+++ JavaScript/Lec3/arrays/ArraysConcate.js
@@ -0,0 +1,5 @@
+let a=[1,2,3,4,5,6,7,8];
+let b=[9,10,11,12,13,14,15,16];
+
+let concatarr=a.concat(b);
+console.log(concatarr); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ]   // Concatenation of two arrays
