1b29ff71d90d007c37cece84b273eab1fc35ddfc
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-03-24 17:11:11 +0530

    Js Arrays

---
+++ JavaScript/Lec3/StringMethods/ConcatStr.js
@@ -0,0 +1,5 @@
+let a="PRATHAMESH";
+let b="GUNJAL";
+
+console.log(a.concat(b)); // PRATHAMESHGUNJAL   // Concatenation of two strings
+console.log(a.concat(" ",b)); // PRATHAMESH GUNJAL   // Concatenation of two strings with space
