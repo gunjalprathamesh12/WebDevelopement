233605ea3e14eeeb889cb6760780639cbaa77b68
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2025-06-05 22:04:16 +0530

    .

---
+++ JavaScript/Lec3/Questions/eleexist.js
@@ -0,0 +1,9 @@
+var arrrays=[1,2,3,4,,5,6,7,8];
+var no=6;
+for(var i=0;i<arrrays.length;i++){
+    if(arrrays[i]===no){
+        console.log("Element exists at index "+i);
+        break;
+    }
+}
+
