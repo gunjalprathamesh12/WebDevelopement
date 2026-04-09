cbaf9b1f85282217dfe6086bde584c4c41fc7746
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-04-09 22:12:30 +0530

    .

---
+++ JavaScript/Lec1/variables/sc.js
@@ -0,0 +1,8 @@
+let a = 10;
+if (a >= 10) {
+    let b=100;
+  console.log("a is greater than or equal to 10");
+  console.log(a);
+}
+
+console.log(b); // This will throw an error because 'b' is not defined outside the if block
---
+++ JavaScript/Lec1/variables/scr.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=29;
+let c="hi";
+let d=90.090;
+console.log(a)
+console.log(b)
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(typeof a);
+console.log(typeof c);
+console.log(typeof d);
