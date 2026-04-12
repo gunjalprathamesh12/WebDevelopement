c3cd0fc59ccf8f878c506ef880d056d456531228
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-04-12 20:53:08 +0530

    js

--- JavaScript/lec2/Templete Literals/script.js
+++ JavaScript/lec2/Templete Literals/script.js
@@ -7,3 +7,17 @@ console.log(`The sum of ${a} and ${b} is ${sum}`);
 let pencile=10;
 let eraser=5;
 console.log(`The Price of Pencile is ${pencile} and the price of eraser is ${eraser}`);
+
+let name="John";
+let age=30;
+console.log(`my name is ${name} and I am ${age} years old`);
+
+//templete literals can also be used to create multi-line strings
+let multiLineString=`This is a multi-line string.
+It can span multiple lines.`;
+console.log(multiLineString);
+
+let exp=10;
+let ss=90;
+console.log("price of exp is"+exp+"and the price of ss is"+ss);
+    
