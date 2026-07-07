81090ef7fa1497b5134f34ad6309d95b46dbe2bb
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-07-07 17:57:57 +0530

    js

---
+++ JavaScript/Lec 6/function expressions/script.js
@@ -0,0 +1,7 @@
+//function expressions is a way to define a function in a variable. It is also called anonymous function because it does not have a name.
+
+let sum = function (a, b) {
+  return a + b;
+};
+
+console.log(sum(1, 2));
