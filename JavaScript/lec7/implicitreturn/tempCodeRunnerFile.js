32028c8f06af40b9fddf42ecf76dbe9c4972b4d0
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-07-08 12:04:48 +0530

    js

---
+++ JavaScript/lec7/implicitreturn/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let a = (a, b) => {
+  return a * b;
+};
+console.log(a(5, 6));
+
+const mul = (a, b) => a * b;
+console.log(mul(5, 6));
+
+const iseven = (n) => n % 2 === 0;
+console.log(iseven(7));
