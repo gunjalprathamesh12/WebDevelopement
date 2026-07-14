3d827afb75c93e2fb2832e4c348935170d6ffae0
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-07-14 17:52:55 +0530

    js revision

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,6 +1,7 @@
-    window.alert("hello");
+// window.alert("hello");
 
-    console.error("you are ok");
-    console.warn("are you gay");
-    console.table([1, 2, 3]);
-    
+// console.error("you are ok");
+// console.warn("are you gay");
+// console.table([1, 2, 3]);
+
+let value = window.prompt("enter a value");
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,50 +1,92 @@
-// // let score = 50;
-// // console.log(score);
-// // score = 90;
-// // console.log(score);
-// // const pi = 3.14;
-// // pi = 3.15;
-// // console.log(pi);
+// // // // // // let score = 50;
+// // // // // // console.log(score);
+// // // // // // score = 90;
+// // // // // // console.log(score);
+// // // // // // const pi = 3.14;
+// // // // // // pi = 3.15;
+// // // // // // console.log(pi);
 
-// const arr = [1, 2, 3, 4];
-// arr.push(5);
-// arr.push(6);
-// console.table(arr);
+// // // // // const arr = [1, 2, 3, 4];
+// // // // // arr.push(5);
+// // // // // arr.push(6);
+// // // // // console.table(arr);
 
-// var a = 10;
-// var a = 20;
-// console.log(a);
+// // // // // var a = 10;
+// // // // // var a = 20;
+// // // // // console.log(a);
 
-// for (var i = 0; i < 3; i++) {
-//   setTimeout(() => {
-//     console.log(i);
-//   }, 1000);
+// // // // // for (var i = 0; i < 3; i++) {
+// // // // //   setTimeout(() => {
+// // // // //     console.log(i);
+// // // // //   }, 1000);
+// // // // // }
+
+// // // // // let a = 20;
+// // // // // console.log(typeof a);
+// // // // // let b = "prathamesh";
+// // // // // console.log(typeof b);
+// // // // // let c = 1.11;
+// // // // // console.log(typeof c);
+// // // // // let d = true;
+// // // // // console.log(typeof d);
+// // // // // let e = null;
+// // // // // console.log(typeof e);
+// // // // // let f = 123n;
+// // // // // console.log(typeof f);
+
+// // // // // let arr = [1, 2, 3, 4];
+// // // // // console.log(typeof arr);
+
+// // // // // let stud = {
+// // // // //   name: "ravi",
+// // // // //   age: 90,
+// // // // //   salary: 90000,
+// // // // // };
+// // // // // console.log(typeof stud);
+
+// // // // // let aa;
+// // // // // let bbb = null;
+// // // // // console.log(typeof aa);
+// // // // // console.log(typeof bbb);
+
+// // // // // let a = 0.9999999999999999999;
+// // // // // console.log(a);
+
+// // // // console.log("5" + 3);
+// // // // console.log("5" - 3);
+// // // // console.log("4" * 4);
+// // // // console.log("10" + 20 + 30);
+
+// // // console.log(typeof NaN);
+
+// // console.log(10 + 5 * 2);
+// // console.log(10 + 19 * 5 - 2);
+
+// // console.log("5" * 2);
+// // console.log("8" - 2);
+// // console.log("12" / 2);
+// // console.log("5" + "5");
+
+// // let name = "prathamesh";
+// // let age = 19;
+// // console.log(`my name is ${name}
+// //      and my
+// //      age
+// //       is :${age}`);
+
+// let a = "APPLE";
+// let b = "APPLE";
+// if (a == b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
 // }
 
-let a = 20;
-console.log(typeof a);
-let b = "prathamesh";
-console.log(typeof b);
-let c = 1.11;
-console.log(typeof c);
-let d = true;
-console.log(typeof d);
-let e = null;
-console.log(typeof e);
-let f = 123n;
-console.log(typeof f);
-
-let arr = [1, 2, 3, 4];
-console.log(typeof arr);
-
-let stud = {
-  name: "ravi",
-  age: 90,
-  salary: 90000,
-};
-console.log(typeof stud);
-
-let aa;
-let bbb = null;
-console.log(typeof aa);
-console.log(typeof bbb);
+let choice = 1;
+switch (choice) {   
+  case 1:
+    console.log("choosed one");
+    break;
+  default:
+    console.log("wrong choice");
+}
