a2c2ec7588cd04ca4d8881b776a03ea60d7172ad
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-08-30 11:59:56 +0530

    require and import

---
+++ Back-end/backend 1 node js/requireandimport/script.js
@@ -0,0 +1,7 @@
+import { sum, sub } from "./maths.js";
+console.log(sum(1, 2));
+console.log(sum(3, 10));
+
+//either in a project we can only use imprt or required not both
+//but if we are using imort then we have to first initialize package.jason
+//inside package.jason after initilizing all we also have to write "type":"module"
