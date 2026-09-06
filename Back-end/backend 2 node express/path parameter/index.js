621ba924e63ef0bd5cf76e9587a975c9c6f82aef
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-09-06 16:59:39 +0530

    path parameters

---
+++ Back-end/backend 2 node express/path parameter/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello i am robot");
+});
+app.get("/:username/:id", (req, res) => {
+  console.log(req.params);
+  res.send("hello i am robot");
+});
