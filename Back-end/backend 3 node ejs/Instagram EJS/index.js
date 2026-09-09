8fb03f5837ca19668d561be7798625fba2f12d37
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2026-09-09 07:28:40 +0530

    created a simple instagram EJS page whihc shows page belongs to whom

---
+++ Back-end/backend 3 node ejs/Instagram EJS/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  //   console.log("home page");
+  res.render("home.ejs"); //or we can write home also
+});
+
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  res.render("instagram.ejs", { username: username });
+});
+app.get("/roldice", (req, res) => {
+  const diceval = Math.floor(Math.random() * 6) + 1;
+  res.render("roldice.ejs", { num: diceval }); //or we can write home also
+});
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
